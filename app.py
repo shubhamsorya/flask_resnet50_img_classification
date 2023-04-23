dd7bf66951e7c6f0085121f42693407df90e1a36
shubhamsorya <shubhamsorya20@gmail.com>
2023-04-23 16:13:02 +0530

    first commit

--- app.py
+++ app.py
@@ -1,85 +1,78 @@
-from __future__ import division, print_function
-# coding=utf-8
-import sys
+
 import os
-import glob
-import re
 import numpy as np
-
-# Keras
-from keras.applications.imagenet_utils import preprocess_input, decode_predictions
-from keras.models import load_model
-from keras.preprocessing import image
-
+import time
 # Flask utils
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 
-# Define a flask app
-app = Flask(__name__)
+# keras
 
-# Model saved with Keras model.save()
-MODEL_PATH = 'models/model_resnet.h5'
-print(" MODEL_PATH :", MODEL_PATH)
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
 
-
-def model_predict(img_path, model):
-    img = image.load_img(img_path, target_size=(224, 224))
-
-    # Preprocessing the image
-    x = image.img_to_array(img)
-    # x = np.true_divide(x, 255)
-    x = np.expand_dims(x, axis=0)
-
-    # Be careful how your trained model deals with the input
-    # otherwise, it won't make correct prediction!
-    x = preprocess_input(x, mode='caffe')
-
-    preds = model.predict(x)
-    return preds
+import tensorflow as tf
+from tensorflow.keras.applications.resnet import ResNet50
+from tensorflow.keras.applications.resnet import preprocess_input
+from tensorflow.keras.applications.resnet import decode_predictions
 
 
-@app.route('/', methods=['GET'])
-def index():
-    # Main page
+#Define a flask_app
+app  = Flask(__name__)
+
+
+
+
+def model_predict(img_file):
+
+    #first save the image in cwd, then reload with target size in next step
+    # Get the filename
+    filename = secure_filename(img_file.filename)
+    img_path = os.path.join(os.getcwd(),filename)
+    img_file.save(img_path)
+
+    # load an image from file
+    image = load_img(img_path, target_size=(224, 224))
+    # convert the image pixels to a numpy array
+    image = img_to_array(image)
+    # reshape data for the model
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    # prepare the image for the VGG model
+    image = preprocess_input(image)
+    # load the model
+    model = ResNet50()
+    # predict the probability across all output classes
+    t1 = time.time()
+    yhat = model.predict(image)
+    t2 = time.time()
+    print('time: {}'.format(t2 - t1))
+    # convert the probabilities to class labels
+    label = decode_predictions(yhat)
+    # retrieve the most likely result, e.g. highest probability
+    label = label[0][0]
+    # print the classification
+    return label[1], label[2]*100
+
+@app.route('/',methods = ['GET'])
+def home():
     return render_template('index.html')
 
-
-@app.route('/predict', methods=['GET', 'POST'])
-def upload():
+@app.route('/predict', methods = ['POST','GET'])
+def predict():
     if request.method == 'POST':
-        # Get the file from post request
-        f = request.files['file']
 
-        # Save the file to ./uploads
-        basepath = os.path.dirname(__file__)
-        file_path = os.path.join(
-            basepath, 'uploads', secure_filename(f.filename))
-        f.save(file_path)
+        # get the file form request
+        img_file = request.files['file']
 
-        # Load your trained model
-        model = load_model(MODEL_PATH)
-        print("*** Model Loaded ***")
-        model._make_predict_function()
+        class_name, prob = model_predict(img_file)
 
-        # Make prediction
-        preds = model_predict(file_path, model)
-        print('*** preds ***')
-
-        # Process your result for human
-        # pred_class = preds.argmax(axis=-1)            # Simple argmax
-        pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
-        print('*** pred class ***')
-        result = str(pred_class[0][0][1])               # Convert to string
-        print('*** result ***')
-        return result
+        return str(class_name)
+    
     elif request.method == 'GET':
         return render_template('index.html')
     return None
 
-
 if __name__ == '__main__':
     print('*** App Started ***')
     app.run(debug=True)
-
